f058bbdd700e9037f0b540762114148ec89fb6ec
DavidRZuluaga <git@davidrzuluaga.com>
2018-11-12 15:33:20 -0500

    ejeercicio v1.1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       tasks: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato'],
       newTask: ""
